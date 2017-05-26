72206ce19b343420b6fd57d233ed4e7b3724fbfa
Dmitry <dima.apanasik@yandex.ru>
2017-05-26 12:12:35 +0300

    new menu

--- courspaper/courspaper/Menu.cpp
+++ courspaper/courspaper/Menu.cpp
@@ -1,114 +1,211 @@
 #include "Header.h"
-HHOOK MousHook;
+#include<string>
+void SendMsg(struct Data *data)
+{
+	if (send(data->Sock, data->Massage, strlen(data->Massage) + 1, 0) != SOCKET_ERROR)
+			printf("Sent!\n");
+		else printf("Error of sending!\n");
+}
 void Menu(struct Data *data)
 {
 	setlocale(LC_CTYPE, "russian");
+	char command[100];
 	char path[100];
-	int Menu = 100;
-	do {
-		printf("1 - reboot the PC\n");
-		printf("2 - shutdown the PC\n");
-		printf("3 - Exit of systeme PC\n");
-		printf("4 - Open CDROM in PC\n");
-		printf("5 - Exit\n\n");
-		printf("8-createfile\n");
-		printf("7-delete file\n");
+	while (1) {
 
-		printf("1 - get command\n0 - close the programm ");
-		scanf("%i", &Menu);
-		switch (Menu)
-		{
-		case 1: {
-			printf("enter command ");
-			scanf_s("%199s", data->Massage, 200);
-			switch (atoi(data->Massage))
-			{
-			/*case 1:
-				printf("%s", data->Massage);
-				printf("enter path");
-				scanf("%s", path);
-				strcat(data->Massage, path);
-				break;*/
-			case 2:
-			{
-				printf("%s", data->Massage);
-				printf("enter path");
-				scanf("%s", path);
-				strcat(data->Massage, path);
-				break;
-			}
-			case 3:
-			{
-				printf("%s", data->Massage);
-				printf("enter path");
-				scanf("%s", path);
-				strcat(data->Massage, path);
-				break;
-			}
-			case 1:
-				{
-				printf("%s", data->Massage);
-				printf("enter path");
-				scanf("%s", path);
-				strcat(data->Massage, path);
-				break;
-			}
-			case 4:
-			{
-				printf("%s", data->Massage);
-				printf("enter path");
-				scanf("%s", path);
-				strcat(data->Massage, path);
-				break;
-			}
-			default: 
-			{
-				if (send(data->Sock, data->Massage, strlen(data->Massage) + 1, 0) != SOCKET_ERROR)
-					printf("Sent!\n");
-				else printf("Error of sending!\n");
-				break;
-			}
+		//scanf("%i", &Menu);
+		//switch (Menu)
+		//{
+		//case 1: {
+		//	printf("enter command ");
+		//	scanf_s("%199s", data->Massage, 200);
+		//	switch (atoi(data->Massage))
+		//	{
+		//	/*case 1:
+		//		printf("%s", data->Massage);
+		//		printf("enter path");
+		//		scanf("%s", path);
+		//		strcat(data->Massage, path);
+		//		break;*/
+		//	case 2:
+		//	{
+		//		printf("%s", data->Massage);
+		//		printf("enter path");
+		//		scanf("%s", path);
+		//		strcat(data->Massage, path);
+		//		break;
+		//	}
+		//	/*case 3:
+		//	{
+		//		printf("%s", data->Massage);
+		//		printf("enter path");
+		//		scanf("%s", path);
+		//		strcat(data->Massage, path);
+		//		break;
+		//	}*/
+		//	case 3:
+		//	{
+		//		if (send(data->Sock, data->Massage, strlen(data->Massage) + 1, 0) != SOCKET_ERROR)
+		//			printf("Sent!\n");
+		//		else printf("Error of sending!\n");
+		//		do {
+		//			recv(data->Sock, data->Massage, 100, 0);
+		//			printf("%s\n", data->Massage);
+		//		} while (strcmp(data->Massage, "Program Manager") != 0);
+		//		break;
+		//	}
+		//	case 1:
+		//		{
+		//		printf("%s", data->Massage);
+		//		printf("enter path");
+		//		scanf("%s", path);
+		//		strcat(data->Massage, path);
+		//		break;
+		//	}
+		//	case 4:
+		//	{
+		//		printf("%s", data->Massage);
+		//		printf("enter path");
+		//		scanf("%s", path);
+		//		strcat(data->Massage, path);
+		//		break;
+		//	}
+		///*	default: 
+		//	{
+		//		if (send(data->Sock, data->Massage, strlen(data->Massage) + 1, 0) != SOCKET_ERROR)
+		//			printf("Sent!\n");
+		//		else printf("Error of sending!\n");
+		//		break;
+		//	}*/
+		//}
+		//	break;
+		//}
+		//case 2:
+		//{
+		//	printf("enter command ");
+		//	scanf_s("%199s", data->Massage, 200);
+		//	switch (atoi(data->Massage))
+		//	{
+		//	case 1:
+		//	{
+		//		printf("enter path for downloading");
+		//		scanf("%s", path);
+		//		recvFile(data, path);
+		//		printf("file downloaded!");
+		//		break;
+		//	}
+		//	case 2: 
+		//	{
+		//		system("cls");
+		//		recv(data->Sock, data->Massage, 387, 0);
+		//		printf("%s\n", data->Massage);
+		//		break;
+		//	}
+		//	case 3:
+		//	{
+		//		do {
+		//			recv(data->Sock, data->Massage, 100, 0);
+		//			printf("%s\n", data->Massage);
+		//		} while (strcmp(data->Massage, "Program Manager") != 0);
+		//		break;
+		//	}
+		//	default: {
+		//		recv(data->Sock, data->Massage, 100, 0);
+		//		printf("%s\n", data->Massage);
+		//		break;
+		//	}
+		//	}
+		//	break;
+		//}
+		//case 3:
+		//{
+		//	mouseClick(data);
+		//	break;
+
+		//}
+		//}
+		//if (send(data->Sock, data->Massage, strlen(data->Massage) + 1, 0) != SOCKET_ERROR)
+		//	printf("Sent!\n");
+		//else printf("Error of sending!\n");
+		printf("$ ");
+		scanf("%s", &command);
+		if (!strcmp(command, "mkfile")) {
+			scanf("%s", &command);
+			strcpy(data->Massage, "1");
+			strcat(data->Massage, command);
+			SendMsg(data);
 		}
-			break;
+		else if (!strcmp(command, "delfile")) {
+			scanf("%s", &command);
+			strcpy(data->Massage, "2");
+			strcat(data->Massage, command);
+			SendMsg(data);
 		}
-		case 2:
-		{
-			printf("enter command ");
-			scanf_s("%199s", data->Massage, 200);
-			switch (atoi(data->Massage))
-			{
-			case 1:
-			{
-				printf("enter path for downloading");
-				scanf("%s", path);
-				recvFile(data, path);
-				printf("file downloaded!");
-				break;
-			}
-			case 2: 
-			{
-				system("cls");
-				recv(data->Sock, data->Massage, 387, 0);
-				printf("%s\n", data->Massage);
-				break;
-			}
-			default: {
-				recv(data->Sock, data->Massage, 100, 0);
-				printf("%s\n", data->Massage);
-				break;
-			}
-			}
-			break;
+		else if (!strcmp(command, "execpr")) {
+			scanf("%s", &command);
+			strcpy(data->Massage, "3");
+			strcat(data->Massage, command);
+			SendMsg(data);
 		}
-		case 3:
-		{
+		else if (!strcmp(command, "closepr")) {
+			scanf("%s", &command);
+			strcpy(data->Massage, "4");
+			strcat(data->Massage, command);
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "getfile")) {
+			scanf("%s", &command);
+			strcpy(data->Massage, "5");
+			strcat(data->Massage, command);
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "getcursor")) {
+			strcpy(data->Massage, "6");
+			SendMsg(data);
 			mouseClick(data);
-			break;
-
 		}
+		else if (!strcmp(command, "getsystinfo")) {
+			strcpy(data->Massage, "7");
+			SendMsg(data);
 		}
-		if (send(data->Sock, data->Massage, strlen(data->Massage) + 1, 0) != SOCKET_ERROR)
-			printf("Sent!\n");
-		else printf("Error of sending!\n");
-	} while (Menu != 0);
+		else if (!strcmp(command, "getinfotasks")) {
+			strcpy(data->Massage, "8");
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "reboot")) {
+			strcpy(data->Massage, "9");
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "shdown")) {
+			strcpy(data->Massage, "a");
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "exitsyst")) {
+			strcpy(data->Massage, "b");
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "opencd")) {
+			strcpy(data->Massage, "c");
+			SendMsg(data);
+		}
+		else if (!strcmp(command, "exit")) {
+			exit(0);
+		}
+		else if (!strcmp(command, "cls")) {
+			system("cls");
+		}
+		else if (!strcmp(command, "showfile")) {
+			strcpy(data->Massage, "d");
+			SendMsg(data);
+			do {
+				recv(data->Sock, data->Massage, 100, 0);
+				if (strcmp(data->Massage, "0") == 0) break;
+				printf("%s\n", data->Massage);
+			} while (1);
+			continue;
+		}
+		else printf(" Unknown command");
+		recv(data->Sock, data->Massage, 100, 0);
+		printf("%s\n", data->Massage);
+	}
 }
--- courspaper/courspaper/Server.cpp
+++ courspaper/courspaper/Server.cpp
@@ -8,6 +8,9 @@ void main()
 	GetConsoleTitle(Title, 1024); // Узнаем имя окна
 	hwnd = FindWindow(NULL, Title); // Узнаем hwnd окна
 	MoveWindow(hwnd, 0, 0, 800, 500, TRUE);
+
+	CreateDirectory("C:\\RemoteAdministration", NULL);
+
 	CreatSocketAndConnect(&data);
 
 	Menu(&data);
